6ac5a8112076671974f7ec44e4cd4d84f0fdcd20
LeifChen <leifchen90@gmail.com>
2021-07-31 18:39:17 +0800

    feat: add import & fact

--- src/main/java/com/chen/drools/RulesHello.java
+++ src/main/java/com/chen/drools/RulesHello.java
@@ -1,5 +1,6 @@
 package com.chen.drools;
 
+import com.chen.drools.bean.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
@@ -19,7 +20,12 @@ public class RulesHello {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
         KieSession ks = kc.newKieSession("helloDrools");
+        Person person = new Person();
+        person.setName("LeifChen");
+        person.setAge(18);
+        ks.insert(person);
         int count = ks.fireAllRules();
+        log.info("修改后的对象:{}", person);
         log.info("总执行了{}条规则", count);
         ks.dispose();
     }
---
+++ src/main/java/com/chen/drools/bean/Person.java
@@ -0,0 +1,17 @@
+package com.chen.drools.bean;
+
+import lombok.Data;
+
+/**
+ * Person
+ * <p>
+ *
+ * @Author LeifChen
+ * @Date 2021-07-31
+ */
+@Data
+public class Person {
+
+    private String name;
+    private Integer age;
+}
