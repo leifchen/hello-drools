01a158a001b2db56218f6a49bf97225034fddfc1
LeifChen <leifchen90@gmail.com>
2021-09-01 09:05:28 +0800

    refactor: add @Before & @After to remove cartesian production

--- src/test/java/com/chen/drools/service/ConditionTest.java
+++ src/test/java/com/chen/drools/service/ConditionTest.java
@@ -3,9 +3,11 @@ package com.chen.drools.service;
 import com.chen.drools.bean.Person;
 import com.chen.drools.bean.School;
 import org.drools.core.base.RuleNameEqualsAgendaFilter;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kie.api.KieBase;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +29,7 @@ import static org.junit.Assert.assertEquals;
 public class ConditionTest {
 
     @Autowired
+    private KieBase kieBase;
     private KieSession kieSession;
 
     @Before
@@ -36,32 +39,39 @@ public class ConditionTest {
         Person person = new Person();
         person.setName("LeifChen");
         person.setClassName("一班");
+
+        kieSession = kieBase.newKieSession();
         kieSession.insert(school);
         kieSession.insert(person);
     }
 
+    @After
+    public void after() {
+        kieSession.dispose();
+    }
+
     @Test
     public void testContains() {
-        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testContains"), 1));
+        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testContains")));
     }
 
     @Test
     public void testMemberOf() {
-        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testMemberOf"), 1));
+        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testMemberOf")));
     }
 
     @Test
     public void testMatches() {
-        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testMatches"), 1));
+        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testMatches")));
     }
 
     @Test
     public void testSoundslike() {
-        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testSoundslike"), 1));
+        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testSoundslike")));
     }
 
     @Test
     public void testStr() {
-        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testStr"), 1));
+        assertEquals(1, kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("testStr")));
     }
 }
